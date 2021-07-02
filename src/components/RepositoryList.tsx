2a3c442286699a76a7f25b33ed693336f44b6345
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2021-07-01 22:13:15 -0300

    Transform typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { useState, useEffect } from 'react';
 import { RepositoryItem } from './RepositoryItem';
 import '../styles/repositories.scss';
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [respositories, setRepositories] = useState([]);
+  const [respositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
